bb8cfdcc65ed5f8a6fed590c1619c56064a336c6
Rahul Awasthi <awasthir@pm.me>
2022-01-04 02:31:23 +0530

    solved Single Number Problem

---
+++ leetcode/bit_manipulation/SingleNumber.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file SingleNumber.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * Try out the problem here:
+ * https://leetcode.com/problems/single-number/
+ * 
+ */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
