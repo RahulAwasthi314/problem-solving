f04a597a5039e6c90205c58da8c9ab0b2df5f1f7
Rahul Awasthi <awasthir@pm.me>
2022-01-04 15:04:18 +0530

    reduced some runtime

--- leetcode/BinaryTree/MergeTwoBinaryTrees.cpp
+++ leetcode/BinaryTree/MergeTwoBinaryTrees.cpp
@@ -45,3 +45,18 @@ public:
         return root;
     }
 };
+
+// reduced approach
+class Solution2 {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        TreeNode* root = nullptr;
+        if (root1 == nullptr and root2 == nullptr) return root;
+        if (root1 == nullptr and root2 != nullptr) return root2;
+        if (root1 != nullptr and root2 == nullptr) return root1;
+        root = new TreeNode(root1->val + root2->val);
+        root->left = mergeTrees(root1->left, root2->left);
+        root->right = mergeTrees(root1->right, root2->right);
+        return root;
+    }
+};
