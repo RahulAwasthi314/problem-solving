df11dd4433a9562311a7d2c7a9d7bedc3c2347e5
Rahul Awasthi <awasthir@pm.me>
2022-01-04 14:56:14 +0530

    Merge two Binary Trees. Try to reduce run time..

---
+++ leetcode/BinaryTree/MergeTwoBinaryTrees.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file MergeTwoBinaryTrees.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * Try out the problem here:
+ * https://leetcode.com/problems/merge-two-binary-trees/
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        TreeNode* root = nullptr;
+        if (root1 == nullptr and root2 ==nullptr) {
+            return root;
+        }
+        
+        if (root1 == nullptr and root2 != nullptr) {
+            root = new TreeNode(root2->val);
+            root->left = mergeTrees(nullptr, root2->left);
+            root->right = mergeTrees(nullptr, root2->right);
+            
+        }
+        if (root1 != nullptr and root2 == nullptr) {
+            root = new TreeNode(root1->val);
+            root->left = mergeTrees(root1->left, nullptr);
+            root->right = mergeTrees(root1->right, nullptr);
+        }
+        if (root1 != nullptr and root2 != nullptr) {
+            root = new TreeNode(root1->val + root2->val);
+            root->left = mergeTrees(root1->left, root2->left);
+            root->right = mergeTrees(root1->right, root2->right);
+        }
+        return root;
+    }
+};
