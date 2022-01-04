7fbb8e18055a2df1458b961e423ff808a7d49368
Rahul Awasthi <awasthir@pm.me>
2022-01-04 16:33:21 +0530

    Added Complement of base 10 integer

---
+++ leetcode/bit_manipulation/complementInteger.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file complementInteger.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * Try out the problem here:
+ * https://leetcode.com/problems/complement-of-base-10-integer/
+ * 
+ */
+
+#include <stack>
+#include <iostream>
+using namespace std;
+int bitwiseComplement(int n){
+    int res = 0;
+    stack<int> a;
+    while (n) {
+        a.push((n & 1) ^ 1);
+        n >>= 1;
+    }
+    int size = a.size();
+    for (int i = 0; i < size; i++) {
+        res <<= 1;
+        res |= a.top();
+        a.pop();
+    }
+    return res;
+
+}
+int main() {
+    int n = bitwiseComplement(5);
+    cout << n;
+    return 0;
+
+}
