bf63e5fb8518af1158ca3340ca604128d7e46641
Rahul Awasthi <awasthir@pm.me>
2022-01-07 04:12:48 +0530

    Added hackerrank/C/struct-enums

---
+++ hackerrank/C/structs_enums/boxes_through_tunnel.c
@@ -0,0 +1,59 @@
+/**
+ * @file boxes_through_tunnel.c
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief
+ * Try out the problem here:
+ * https://www.hackerrank.com/challenges/too-high-boxes
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+	/**
+	* Define three fields of type int: length, width and height
+	*/
+    int length;
+    int height;
+    int width;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+	/**
+	* Return the volume of the box
+	*/
+    return b.height * b.width * b.length;
+}
+
+int is_lower_than_max_height(box b) {
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+    if (b.height < MAX_HEIGHT) {
+        int volume = get_volume(b);
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	box *boxes = malloc(n * sizeof(box));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+	}
+	for (int i = 0; i < n; i++) {
+		if (is_lower_than_max_height(boxes[i])) {
+			printf("%d\n", get_volume(boxes[i]));
+		}
+	}
+	return 0;
+}
