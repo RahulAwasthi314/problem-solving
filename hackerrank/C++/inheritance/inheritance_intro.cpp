f6ff8fb487926a357d946896f8f05d0bfaa581c6
Rahul Awasthi <awasthir@pm.me>
2022-01-07 05:00:37 +0530

    Added hackerrannk/C++/inheritance que.

---
+++ hackerrank/C++/inheritance/inheritance_intro.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file inheritance_intro.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * Try out the problem here:
+ * https://www.hackerrank.com/challenges/inheritance-introduction/
+ * 
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+
+class Isosceles : public Triangle{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+  		//Write your code here.
+        void description() {
+            cout << "In an isosceles triangle two sides are equal\n";
+        }
+};
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
+    return 0;
+}
