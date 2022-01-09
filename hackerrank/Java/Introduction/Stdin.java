c01a9f87b2b0af457a92c8e50930ce19464c8687
Rahul Awasthi <awasthir@pm.me>
2022-01-09 10:45:57 +0530

    Stdin.java

---
+++ hackerrank/Java/Introduction/Stdin.java
@@ -0,0 +1,29 @@
+/**
+ * @file Stdin.java
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * Try out the problem here:
+ * https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/
+ *
+ */
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
