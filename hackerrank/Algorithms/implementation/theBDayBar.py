48f5541a757f9f9d9867c8e75fae0f833eefc914
Rahul Awasthi <awasthir@pm.me>
2022-01-17 01:13:59 +0530

    Added theBirthdayBar.py

---
+++ hackerrank/Algorithms/implementation/theBDayBar.py
@@ -0,0 +1,56 @@
+"""
+  @file theBDayBar.py
+  @author Rahul Awasthi (awasthir@pm.me)
+  
+  @brief 
+  try out the problem here:
+    https://www.hackerrank.com/challenges/the-birthday-bar
+    
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+
+
+def birthday(s, d, m):
+    # Write your code here
+    count = 0
+    for i in range(len(s)):
+        j = i+m
+        if sum(s[i:j]) == d:
+            count += 1
+        j+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
