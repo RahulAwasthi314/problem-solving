23eca36143a6f834e9feed2e7ff7682d476bfa21
Rahul Awasthi <awasthir@pm.me>
2022-01-30 09:52:59 +0530

    Added countNonLeaf.cpp

---
+++ leetcode/BinaryTree/countNonLeaf.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file countNonLeaf.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * try out the problem here:
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int count(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + count(root->left) + count(root->right);
+    }
+public:
+    int countNodes(TreeNode* root) {
+        return count(root);
+    }
+};
