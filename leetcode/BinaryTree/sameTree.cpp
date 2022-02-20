9547d90014aacb00b9e2251c5aa637884ebc260e
Rahul Awasthi <awasthir@pm.me>
2022-02-21 04:06:03 +0530

    Added sameTree.cpp

---
+++ leetcode/BinaryTree/sameTree.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file sameTree.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * try out the problem here:
+ * https://leetcode.com/problems/same-tree/
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr and q == nullptr) return true;
+        else if (p != nullptr and q != nullptr) {
+            if (p->val == q->val) 
+                return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+            else return false;
+        }
+        else {
+            return false;
+        }
+    }
+};
