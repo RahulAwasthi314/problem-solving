a3c53bf955b22a6ba6c60dc775595008bcc17f90
Rahul Awasthi <awasthir@pm.me>
2022-03-12 23:33:22 +0530

    transposed the matrix

---
+++ leetcode/matrix/transposeMatrix.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file transposeMatrix.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ * 
+ * @brief 
+ * try out the problem here:
+ * https://leetcode.com/problems/transpose-matrix/
+ * 
+ */
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> vec (n, vector<int> (m, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                vec[j][i] = matrix[i][j];
+            }
+        }
+        return vec;
+    }
+};
