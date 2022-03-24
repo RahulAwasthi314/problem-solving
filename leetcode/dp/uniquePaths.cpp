07972d056e8ade9a1e94c372713383f072dfc300
Rahul Awasthi <awasthir@pm.me>
2022-03-24 23:40:18 +0530

    Added uniquePaths.cpp

--- leetcode/dp/uniquePaths.cpp
+++ leetcode/dp/uniquePaths.cpp
@@ -36,3 +36,65 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+class Solution2 {
+    
+    // recursive approach to solve the problem  taking exponential time
+    // time: O(2^(m+n))    space: O(1)  stack_space: O(m+n)
+    int recursive(int m, int n) {
+        if (m == 0 or n == 0) return 1;
+        return recursive(m, n-1) + recursive(m-1, n);
+    }
+    
+    
+    // memoization procedure
+    int soln_memoize(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        dp[0][0] = 0;
+        return memoize(m-1, n-1, dp);
+    }
+    int memoize(int m, int n, vector<vector<int>>& dp) {
+        if (m == 0 or n == 0) return 1;
+        if (dp[m][n] != -1) return dp[m][n];
+        return memoize(m-1, n, dp) + memoize(m, n-1, dp);
+    }
+    
+
+    void printp(int m, int n, vector<vector<int>>& dp) { 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << dp[i][j] << " ";
+            } cout << "\n";
+        }
+    }
+    
+    
+    // tabular approach  O(m*n)
+    int tabular(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        // printp(m, n, dp);
+        return dp[m-1][n-1];
+    }
+    
+    
+    // tabular appraoch O(2*n)
+    
+    // tabular appraoch O(n)
+    
+public:
+    int uniquePaths(int m, int n) {
+        
+        // return recursive(m-1, n-1);
+        
+        // return soln_memoize(m, n);
+        
+        return tabular(m, n);
+    }
+};
