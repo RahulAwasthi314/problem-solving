fe71a41453b8da27ef4e3a8ccc10eae4c7649874
Rahul Awasthi <awasthir@pm.me>
2022-04-15 23:00:05 +0530

    Added lcaOfBinaryTree.cpp

---
+++ leetcode/BinaryTree/lcaOfBinaryTree.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file lcaOfBinaryTree.cpp
+ * @author Rahul Awasthi (awasthir@pm.me)
+ *
+ * @brief
+ * try out the problem here:
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root or root == p or root == q) return root;
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+        if (left and right) return root;
+        if (left) return left;
+        return right;
+    }
+};
