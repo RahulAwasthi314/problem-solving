41200e76f677f44fccf0e005311a0b77e8c4c346
Rahul Awasthi <awasthir@pm.me>
2022-05-10 19:26:27 +0530

    Added combinationSum3 other solution

--- leetcode/backtracking/combinationSum3.cpp
+++ leetcode/backtracking/combinationSum3.cpp
@@ -1,15 +1,15 @@
 /**
  * @file combinationSum3.cpp
  * @author Rahul Awasthi (awasthir@pm.me)
- * 
- * @brief 
+ *
+ * @brief
  * try out the problem here:
  * https://leetcode.com/problems/combination-sum-iii/
- * 
+ *
  */
 
 class Solution {
-    
+
     void bt(vector<int>& nums, int pos, int k, int target, vector<int>& currSol, vector<vector<int>>& allSol) {
         if (target == 0 and currSol.size() == k) {
             allSol.push_back(currSol);
@@ -31,9 +31,43 @@ class Solution {
         bt(nums, 0, k, target, currSol, allSol);
         return allSol;
     }
-    
+
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         return wrapper_bt(k, n);
+    }
+};
+
+
+class Solution2 {
+
+    void helper(vector<int>& currSol, vector<vector<int>>& allSol, int num, int& currSum, int n, int k) {
+        if (currSol.size() == k and currSum == n) {
+            allSol.push_back(currSol);
+            return;
+        }
+        if (num == 10) return;
+        if (currSum > n) return;
+
+        currSum += num;
+        currSol.push_back(num);
+        helper(currSol, allSol, num+1, currSum, n, k);
+        currSol.pop_back();
+        currSum -= num;
+        helper(currSol, allSol, num+1, currSum, n, k);
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> currSol;
+        vector<vector<int>> allSol;
+        int currSum = 0;
+        for (int i = 1; i < 10; i++) {
+            currSum += i;
+            currSol.push_back(i);
+            helper(currSol, allSol, i+1, currSum, n, k);
+            currSol.pop_back();
+            currSum -= i;
+        }
+        return allSol;
     }
 };
