a472b2d3122464fe922b915e8c2302c036206c5c
Rahul Awasthi <awasthir@pm.me>
2022-05-16 01:06:04 +0530

    Added minDistancebwTwoNodes dfs approach

--- leetcode/BinaryTree/minDistancebwTwoNodes.cpp
+++ leetcode/BinaryTree/minDistancebwTwoNodes.cpp
@@ -45,3 +45,37 @@ public:
         return bruteforce(root);
     }
 };
+
+
+class Solution {
+    int ans = INT_MAX;
+    int cnt = 0;
+    int prevInorder = 0;
+     void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+
+        if (cnt != 0) {
+            ans = min(ans, root->val - prevInorder);
+        }
+         prevInorder = root->val;
+         ++cnt;
+        inorder(root->right);
+    }
+
+public:
+    int minDiffInBST(TreeNode* root) {
+        inorder(root);
+        return ans;
+    }
+};
+/*
+[4,2,6,1,3]
+[1,0,48,null,null,12,49]
+[15,10,18,8,14]
+[15,10,18,8,13,16]
+[1,null,2]
+[3,null,4]
+[0,null,1]
+[2,1,null]
+*/
